9d31fde9990e890dc96ba0ceb515b0bcd6101ee4
KozelVolodumur <Volodumurvel@gmail.com>
2022-07-08 15:32:23 +0300

    finished up contact routes

--- api/controllers/contacts-controller.js
+++ api/controllers/contacts-controller.js
@@ -1,42 +1,98 @@
+const { rmSync } = require('fs');
 const Contact = require('../models/contact-model');
 
 class ContactsController {
     getAll = () => {
-        return (req, res, next) => {
+        return async (req, res, next) => {
+            const userId = req.userData.id;
+            const {count, rows} = await Contact.findAndCountAll({
+                where: {user_id: userId},
+                order: [['lname', 'ASC'], ['fname', 'ASC']]
+            });
             res.status(200).json({
-                sucess: true,
-                data: [
-                    {fname: "Tony", lname: "Pagt", phone: "555-111-1234"},
-                    {fname: "Ben", lname: "Rfgh", phone: "555-111-7867"}
-                ]
-            })
+                data: rows,
+                total: count,
+                success: true
+            });
         }
     }
 
     create = () => {
-        return(req, res, next) =>{
-            console.log(req);
-            const obj = {fname: req.body.fname, lname: req.body.lname, email: req.body.email}
-            res.status(200).json({success:true, method: "create", contact: obj});
+        return async (req, res, next) =>{
+            try{
+                const userId = req.userData.id;
+                const contact = await Contact.create({
+                    fname: req.body.fname,
+                    lname: req.body.lname,
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    user_id: userId
+                });
+                res.status(200).json({
+                    success: true,
+                    contact: contact
+                });
+            } catch(err){
+                res.status(422).json(err.error);
+            }
         }
     }
 
     findById = () => {
-        return (req, res, next) => {
-            //console.log(req.query);
-            res.status(200).json({success:true, method: "findById", contactId: req.params.id});
+        return async (req, res, next) => {
+            const userId = req.userData.id;
+            const contactId = req.params.id;
+            const contact = await Contact.findOne({
+                where: {id: contactId, user_id: userId}
+            });
+            const resp = {success: false, contact: null};
+            if(contact){
+                resp.success = true;
+                resp.contact = contact;
+            }
+            res.status(200).json(resp);
         }
     }
 
     update = () => {
-        return (req, res, next) => {
-            res.status(200).json({success:true, method: "update"});
+        return async (req, res, next) => {
+           const contactId = req.params.id;
+           const userId = req.userData.id;
+           const resp = {success: false, contact: null, msg: "Contact not found."};
+           const contact = await Contact.findOne({
+               where: {id: contactId, user_id: userId}
+           });
+           if(contact){
+               const vals = {
+                   fname: req.body.fname,
+                   lname: req.body.lname,
+                   email: req.body.email,
+                   phone: req.body.phone
+               }
+               await Contact.update(vals, {where: {id: contactId}});
+               await contact.reload();
+               resp.success = true;
+               resp.msg = "Contact updated.";
+               resp.contact = contact;
+           }
+           res.status(200).json(resp);
         }
     }
 
     delete = () => {
-        return (req,res,next) => {
-            res.status(200).json({success:true, method: "delete"});
+        return async (req,res,next) => {
+            const contactId = req.params.id;
+            const userId = req.userData.id;
+            const contact = await Contact.findOne({
+                where: {id: contactId, user_id: userId}
+            });
+            const resp = {success: false, msg: "Contact not found."}
+            if(contact){
+                await contact.destroy();
+                resp.success = true;
+                resp.msg = "Contact deleted."
+            }
+            res.status(200).json(resp);
         }
     }
 }
